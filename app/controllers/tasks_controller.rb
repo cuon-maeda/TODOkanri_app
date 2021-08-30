85477c13cd0b30e07f8bfade2ed910459f483939
maeda <ymaeda@cuon.co/jp>
2021-08-30 18:09:11 +0900

    Rubocopの実行

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,11 +14,10 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-       redirect_to tasks_path, notice: "タスクを登録しました"
+      redirect_to tasks_path, notice: "タスクを登録しました"
     else
       render action: "new"
     end
-
   end
 
   def edit
@@ -35,15 +34,13 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find_by(id: params[:id])  
+    @task = Task.find_by(id: params[:id])
     @task.destroy
     redirect_to tasks_path, alert: "タスクを削除しました"
   end
 
   private
-
-  def task_params
-    params.require(:task).permit(:name, :user)
-  end
-
+    def task_params
+      params.require(:task).permit(:name, :user)
+    end
 end
