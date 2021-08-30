0e40a08e111bab61cfb8d3b0aa2aea726acdb92e
maeda <ymaeda@cuon.co/jp>
2021-08-30 18:05:17 +0900

    タスク作成、編集、削除、バリテーションメッセージ、flashメッセージの実装

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.order(created_at: :desc)
+  end
+
+  def show
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+       redirect_to tasks_path, notice: "タスクを登録しました"
+    else
+      render action: "new"
+    end
+
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました"
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])  
+    @task.destroy
+    redirect_to tasks_path, alert: "タスクを削除しました"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :user)
+  end
+
+end
