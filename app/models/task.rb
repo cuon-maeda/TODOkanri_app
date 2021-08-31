f14af26caedaf87211358e6bf6d1b4dc38e393d2
maeda <ymaeda@cuon.co/jp>
2021-08-31 16:31:46 +0900

    Rubocopの実行

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   validates :name, { presence: true }
   validates :user, { presence: true }
   has_many :tasks_categories
-  has_many :categories, through: :tasks_categories 
+  has_many :categories, through: :tasks_categories
 end
