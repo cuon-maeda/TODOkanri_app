9cc97a517c0cd95629866766658b73f67a059e32
maeda <ymaeda@cuon.co/jp>
2021-09-08 16:08:40 +0900

    updateのテスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
-    sequence(:name) { |n| "TEST_NAME#{n}"}
-    sequence(:user) { |n| "TEST_user#{n}"}
+    sequence(:name) { |n| "TEST_NAME#{n}" }
+    sequence(:user) { |n| "TEST_user#{n}" }
     status { "done" }
   end
 end
