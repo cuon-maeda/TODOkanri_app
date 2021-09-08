2fb6c0e6a9059bc0357573d48c973cbebc98da98
maeda <ymaeda@cuon.co/jp>
2021-09-08 10:44:27 +0900

    createのテスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :task do
     sequence(:name) { |n| "TEST_NAME#{n}"}
     sequence(:user) { |n| "TEST_user#{n}"}
-    sequence(:status) { |n| 1}
+    sequence(:status) { |n| "done"}
   end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :request do
   let!(:task) {create(:task)}
+  let!(:task_params) {attributes_for(:task)}
   describe 'GET /index' do
     context '適切なステータスコードが帰ってくるか' do
       subject { get tasks_path }
@@ -38,9 +39,7 @@ RSpec.describe TasksController, type: :request do
       end
 
       it 'レコードが1つ減ること' do
-        expect do
-          subject
-        end.to change(Task, :count).by(-1)
+        expect {subject}.to change(Task, :count).by(-1)
       end
 
       it 'redirect 先が正しい' do
@@ -48,4 +47,43 @@ RSpec.describe TasksController, type: :request do
       end
     end
   end
+
+  describe 'POST /create' do
+    context 'createコントローラーのテスト 正常形' do
+      subject { post tasks_path, params: { task: task_params }}
+      it '適切なステータスコードが帰ってくる' do
+        is_expected.to eq(302)
+      end
+
+      it 'レコードが1つ増えること' do
+        expect {subject}.to change(Task, :count).by(1)
+      end
+
+      it '作成されたレコードの値が意図したものになっていること' do
+        subject
+        expect(Task.last.id).to eq(task.id+1)
+      end
+
+      it 'redirect 先が正しい' do
+        is_expected.to redirect_to(tasks_path)
+      end
+    end
+    context 'createコントローラーのテスト 異常形' do
+      subject { post tasks_path, params: { task: {name: ''}}}
+      it '適切なステータスコードが帰ってくる' do
+        is_expected.to eq(200)
+      end
+
+      it 'レコードが増えないこと' do
+        expect do
+          subject
+        end.to change(Task, :count).by(0)
+      end
+
+      it 'render 先が正しい' do
+        is_expected.to render_template(:new)
+      end
+    end
+  end
+  
 end
